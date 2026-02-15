d6ddda64f4e85bd50f0bd7b98cf2c6261bbea1a4
Max Stahlke <max.stahlke@gmail.com>
2026-02-15 21:36:28 +0000

    Add relay to publish additional image topics for yolo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,6 @@
 # echo "Starting nodes"
 
-# source /opt/ros/humble/setup.bash
+source /opt/ros/humble/setup.bash
 source /opt/ros_ws/install/setup.bash
 source /workspace/install/setup.bash
 ros2 launch launch_robot realsense_nvblox_nav.launch.py
-#!/bin/bash
-# set -e
-
-# # Library Pfade zuerst
-# export LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu:/usr/local/lib:$LD_LIBRARY_PATH
-
-# # ROS Setup
-# source /opt/ros/humble/setup.bash
-# source /opt/ros_ws/install/setup.bash
-# source /workspace/install/setup.bash
-
-# # Starte ROS Node
-# exec ros2 launch launch_robot realsense_nvblox_nav.launch.py
